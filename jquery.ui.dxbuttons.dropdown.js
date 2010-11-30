df551a302d8af76d1a3ea7762dc1c4923b214d34
Eduard Kracmar <kracmar@dannax.sk>
2010-11-30 23:06:44 +0100

    Removed mouseout method, added clickout overlay

--- jquery.ui.dxbuttons.dropdown.js
+++ jquery.ui.dxbuttons.dropdown.js
@@ -11,47 +11,43 @@
 
                 function initialization() {
 
+		    clickOut.click(function(event){
+			container.hide();
+			clickOut.hide();
+		    });
+
                     $this.after(container);
                     $this.button({
                         icons: {
                             secondary: "ui-icon-triangle-1-s"
                         }
                     }).click(function(event){
-
                         container.toggle();
+
+			if ( container.is(':visible')) {
+			    clickOut.show();
+			}
+
+
                         return false;
-                    }).css('z-index', 2).mouseover(function(event){
-
-                            cointainer.show();
-
-                    });
+                    }).css('z-index', 100);
 
                     var maxWidth = $this.width();
 
                     for ( var indx in list) {
                         var outer = $('<div></div>');
-                        list[indx].mouseover(function(event){
-                            container.show();
-                        });
                         maxWidth = (list[indx].outerWidth(true) > maxWidth) ? list[indx].outerWidth(true) : maxWidth;
-                        outer.append(list[indx]).mouseover(function(event){
-                            container.show();
-                        });
+                        outer.append(list[indx]);
                         container.find('.inner').append(outer);
                     }
                     
-                    container.css('width', maxWidth + 'px').hide().position({
+                    container.css('width', maxWidth + 'px').css('z-index', 99).hide().position({
                         "my": "left top",
                         "at": "left bottom",
                         "collision": "none",
                         "of": $this,
                         "offset": "0 -3"
-                    }).mouseout(function(event){
-                        container.hide();
                     });
-
-                    
-                    
                 }
 
                 /*
@@ -123,7 +119,8 @@
 
                 // setting defaults
                 var $this = $(this),
-                container = $('<div class="dropdown"><div class="inner"></div></div>');
+                container = $('<div class="dropdown"><div class="inner"></div></div>'),
+		clickOut = $('#clickout');
 
                 initialization();
 
--- jquery.ui.dxbuttons.js
+++ jquery.ui.dxbuttons.js
@@ -6,103 +6,111 @@
 (function( $ ){
 
     var methods = {
-        init : function( options ) {
-            return this.each(function(){
+	init : function( options ) {
+	    return this.each(function(){
 
-                // initialization
-                function initialization() {
+		// initialization
+		function initialization() {
 
-                    // all inputs and buttons
-                    $this.find('input[type=submit], button').each(function(){
+		    // all inputs and buttons
+		    $this.find('input[type=submit], button').each(function(){
 
-                        var $this = $(this);
+			var $this = $(this);
 
-                        // create UI button
-                        $this.button();
+			// create UI button
+			$this.button().css('z-index', 99);
 
-                        // create sets collection
-                        var set = $this.attr('set');
-                        if ( set ) {
-                            if ( !setsArry[set]) {
-                                setsArry[set] = [];
-                            }
-                            setsArry[set].push($this);
-                        }
+			// create sets collection
+			var set = $this.attr('set');
+			if ( set ) {
+			    if ( !setsArry[set]) {
+				setsArry[set] = [];
+			    }
+			    setsArry[set].push($this);
+			}
 
-                        // create dropdowns collection
-                        var dropdown = $this.attr('dropdown');
-                        if ( dropdown ) {
-                            if ( !dropdownsArry[dropdown]) {
-                                dropdownsArry[dropdown] = {
-                                    master : null,
-                                    list : []
-                                };
-                            }
+			// create dropdowns collection
+			var dropdown = $this.attr('dropdown');
+			if ( dropdown ) {
+			    if ( !dropdownsArry[dropdown]) {
+				dropdownsArry[dropdown] = {
+				    master : null,
+				    list : []
+				};
+			    }
 
-                            if ( this.tagName == 'BUTTON') {
-                                dropdownsArry[dropdown].master = $this;
-                            } else if ( this.tagName == 'INPUT') {
-                                dropdownsArry[dropdown].list.push($this);
-                            }
-                        }
-                    });
+			    if ( this.tagName == 'BUTTON') {
+				dropdownsArry[dropdown].master = $this;
+			    } else if ( this.tagName == 'INPUT') {
+				dropdownsArry[dropdown].list.push($this);
+			    }
+			}
+		    });
 
-                    // build buttonsets
-                    for (var set in setsArry ) {
-                        var pointer = false;
+		    // build buttonsets
+		    for (var set in setsArry ) {
+			var pointer = false;
 
-                        for (var setIndx in setsArry[set]) {
-                            var button = setsArry[set][setIndx];
+			for (var setIndx in setsArry[set]) {
+			    var button = setsArry[set][setIndx];
 
-                            if ( setIndx == 0) {
-                                pointer = button;
-                                var container = $('<span></span>');
-                                button.after(container);
-                            }
-                            container.append(button);
-                        }
-                        if (pointer) {
-                            container.buttonset();
-                        }
-                    }
+			    if ( setIndx == 0) {
+				pointer = button;
+				var container = $('<span></span>');
+				button.after(container);
+			    }
+			    container.append(button);
+			}
+			if (pointer) {
+			    container.buttonset();
+			}
+		    }
 
-                    // build dropdowns
-                    for (var dropdown in dropdownsArry) {
-                        dropdownsArry[dropdown].master.dxButtonsDropdown(dropdownsArry[dropdown].list );
-                    }
-                }
+		    // build dropdowns
+		    for (var dropdown in dropdownsArry) {
+			dropdownsArry[dropdown].master.dxButtonsDropdown(dropdownsArry[dropdown].list );
+		    }
+		}
 
 
-                // plugin settings
-                var settings = {
-                };
+		// plugin settings
+		var settings = {
+		};
 
-                // extending settings with options
-                if ( options ) {
-                    $.extend( settings, options );
-                }
+		// extending settings with options
+		if ( options ) {
+		    $.extend( settings, options );
+		}
 
-                // setting defaults
-                var $this = $(this),
-                setsArry = [],
-                dropdownsArry = [];
+		// setting defaults
+		var $this = $(this),
+		setsArry = [],
+		dropdownsArry = [];
 
-                // initianization
-                initialization();
-            });
-        }
+		// clickout overlay
+		var clickOut = $('#clickout');
+		if ( !clickOut.length ) {
+		    clickOut = $('<div id="clickout"></div>');
+		    $('body').append(clickOut);
+		}
+		clickOut.width($(window).width()).height($(window).height()).css('z-index', 98).hide();
+
+		// initianization
+		initialization();
+	    });
+	}
     };
 
     $.fn.dxButtons = function( method ) {
 
-        // Method calling logic
-        if ( methods[method] ) {
-            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-        } else if ( typeof method === 'object' || ! method ) {
-            return methods.init.apply( this, arguments );
-        } else {
-            $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
-        }
+	// Method calling logic
+	if ( methods[method] ) {
+	    return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+	} else if ( typeof method === 'object' || ! method ) {
+	    return methods.init.apply( this, arguments );
+	} else {
+	    $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+	}
     };
 
 })( jQuery );
